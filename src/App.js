5dc44ec3a04d60f949dfec5fbf7aec0854abea6e
ge-wijayanto <greg.evan01@gmail.com>
2023-03-31 11:45:24 +0700

    Solve exposure for HSTS, CSP, and other Security Headers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import "./App.css";
 import Login from "./components/Login";
 import NavBar from "./components/NavBar";
+import helmet from "helmet";
+
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "https://apis.google.com"],
+        styleSrc: ["'self'", "https://fonts.googleapis.com"],
+        fontSrc: ["'self'", "https://fonts.gstatic.com"],
+        imgSrc: ["'self'", "data:"],
+      },
+    },
+    hsts: {
+      maxAge: 31536000,
+      includeSubDomains: true,
+      preload: true,
+    }, 
+    referrerPolicy: { policy: "same-origin" },
+  }));
+
 function App() {
 
 const password="v6faF+gC7Zs@?VyCqtEYQ%?S"
